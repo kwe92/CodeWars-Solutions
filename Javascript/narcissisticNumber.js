816ce54e732e3a616f135ce1fd8719f7ed31095f
Kweayon  Clark <Kweayon@gmail.com>
2023-02-19 14:11:57 -0600

    Completed Narcissistic Numbers Challenge

---
+++ Javascript/narcissisticNumber.js
@@ -0,0 +1,29 @@
+// A power function
+function pow(num, expo) {
+  return num ** expo;
+}
+
+// Convert number to string and split the string into an array
+function numToArray(number) {
+  const strNum = number.toString();
+
+  return strNum.split("");
+}
+
+// Return the sum of a number[]
+function reducer(array) {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+}
+
+function narcissistic(number) {
+  const strArray = numToArray(number);
+  const expo = strArray.length;
+  const raisedArray = strArray.map((ele) => pow(parseInt(ele), expo));
+  const sum = reducer(raisedArray);
+
+  return sum === number;
+}
+
+console.log(`Raised Array: ${narcissistic(153)}`);
